aa35f163c76af3dd20987912bea00f93c4070991
enzotrompeneers <trompeneers@telenet.be>
2017-11-02 01:19:22 +0100

    contest manager page centered

--- resources/views/wedstrijdverantwoordelijke.blade.php
+++ resources/views/wedstrijdverantwoordelijke.blade.php
@@ -6,35 +6,29 @@
     @if (Auth::user()->email == $admin_email)
     <form class="form-horizontal" method="POST" action="{{ route('admin.update') }}" data-abide novalidate>
         {{ csrf_field() }}
+        
         <div class="row">
-            <div class="small-6 columns{{ $errors->has('email') ? ' has-error' : '' }}">
-                <h1>Wedstrijdverantwoordelijke</h1>
-                <label>Wedstrijdverantwoordelijke <small>Wijzigen</small>
-                <input type="text" placeholder="naam@email.com" pattern="email" name="email" value="{{ $admin[0] }}">
-                @if ($errors->has('email'))
-                    <strong>{{ $errors->first('email') }}</strong>
-                @endif
-                <small class="error">Geef een geldig e-mail adres</small>
+            <div class="small-12 columns{{ $errors->has('email') ? ' has-error' : '' }}">
+                <div class="call_to_action_wrapper">
+                    <h1>Wedstrijdverantwoordelijke</h1>
+                        <input type="text" placeholder="naam@email.com" pattern="email" name="email" value="{{ $admin[0] }}">
+                        @if ($errors->has('email'))
+                            <strong>{{ $errors->first('email') }}</strong>
+                        @endif
+                        <small class="error">Geef een geldig e-mail adres</small>
 
-                @if(isset($email_exist))
-                    @if(!$email_exist)
-                        <div data-alert class="alert-box alert round">
-                            Wedstrijdverantwoordelijke is niet geregistreerd (niet gevonden).
-                            <a href="#" class="close">&times;</a>
-                        </div>
-                    @endif
-                @endif
-                
-                </label>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="small-6 columns">
-                <button type="submit" class="btn_spotify ">
-                    Wijzigen
-                </button>
-                
+                        @if(isset($email_exist))
+                            @if(!$email_exist)
+                                <div data-alert class="alert-box alert round">
+                                    Wedstrijdverantwoordelijke is niet geregistreerd (niet gevonden).
+                                    <a href="#" class="close">&times;</a>
+                                </div>
+                            @endif
+                        @endif
+                    <button type="submit" class="btn_spotify ">
+                        Wijzigen
+                    </button>
+                </div>
             </div>
         </div>
     </form>
